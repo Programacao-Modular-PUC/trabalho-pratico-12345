d8d9fb268c69fea6bdb77fc837736de7be484498
Davi Santana <1596978@sga.pucminas.br>
2026-05-14 16:17:22 -0300

    Corrige regra de capacidade do quarto duplo

--- hospedagem/src/back-end/src/main/java/com/hospedagem/model/QuartoDuplo.java
+++ hospedagem/src/back-end/src/main/java/com/hospedagem/model/QuartoDuplo.java
@@ -12,19 +12,28 @@ public class QuartoDuplo extends Quarto {
 
     private boolean possuiBerco;
 
-    @Override
-    public double calcularDiaria(int numeroPessoas, boolean solicitouBerco) {
-        if (solicitouBerco && !possuiBerco) {
-            throw new NegocioException("Este quarto duplo não possui berço disponível.");
-        }
-        double adicionalCama = switch (tipoCama) {
-            case CASAL -> 0.0;
-            case QUEEN -> 80.0;
-            case KING  -> 150.0;
-        };
-        double taxaBerco = solicitouBerco ? 40.0 : 0.0;
-        return getValorBase() + adicionalCama + taxaBerco;
+@Override
+public double calcularDiaria(int numeroPessoas, boolean solicitouBerco) {
+    if (numeroPessoas < 1 || numeroPessoas > 2) {
+        throw new NegocioException(
+            "Quarto duplo permite no máximo 2 hóspedes."
+        );
     }
+
+    if (solicitouBerco && !possuiBerco) {
+        throw new NegocioException("Este quarto duplo não possui berço disponível.");
+    }
+
+    double adicionalCama = switch (tipoCama) {
+        case CASAL -> 0.0;
+        case QUEEN -> 80.0;
+        case KING  -> 150.0;
+    };
+
+    double taxaBerco = solicitouBerco ? 40.0 : 0.0;
+
+    return getValorBase() + adicionalCama + taxaBerco;
+}
 
     public TipoCama getTipoCama() { return tipoCama; }
     public void setTipoCama(TipoCama tipoCama) { this.tipoCama = tipoCama; }
